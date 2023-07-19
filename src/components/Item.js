adeeef89430d0dac495c22c993915726cd8022d5
michellemwangi01 <michellemwangim@gmail.com>
2023-07-19 11:57:55 +0300

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [liClassName, setLiClassName] = useState("")
+  const [buttonTextHandler, setButtonTextHandler] = useState(true)
+  const [buttonText, setButtonText] = useState("Add to Cart")
+
+
+  const addToCartHandler = ()=>{
+    setButtonTextHandler(!buttonTextHandler)
+    buttonTextHandler ? setLiClassName("in-cart") : setLiClassName("") 
+    buttonTextHandler ? setButtonText("Add to Cart") : setButtonText("Remove From Cart") 
+
+  }
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCartHandler} className="add">{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filteredValue, setFilteredValue] = useState("All")
+ // const [filteredItems, setFilteredItems] = useState(items)
+  const changeValueHandler = (e)=>{
+    setFilteredValue(e.target.value)
+    // setFilteredItems(()=> items.filter(item => item.cate))
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={changeValueHandler} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +20,14 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+      {(filteredValue === "All") ? 
+      items.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        )):
+         items.filter((item)=> item.category === filteredValue).map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
+       
       </ul>
     </div>
   );
